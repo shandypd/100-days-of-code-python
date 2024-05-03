b12030f22a3faf714f9b577efdce3f936da7e52a
Shandy Pranata Darmawan <38228367+shandypd@users.noreply.github.com>
2024-05-03 20:15:08 +0700

    Day 1 - Working with Variables in Python to Manage Data

---
+++ day-1/_notes.py
@@ -0,0 +1,30 @@
+# Print a string
+print("Hello, world!")
+
+# Print quotation marks
+print('She said "Hello" and then left.')
+print("She said \"Hello\" and then left.")
+
+# Print multiple lines of string
+print("Hello, world!\nHello, world!")
+
+# Concatenate strings
+print("Hello, " + "Ben!")
+
+# Get user input
+input("What is your name? ")
+
+# Get the length of a string
+print(len("Hello!"))
+
+# Save data in a variable
+name = "Jack"
+print(name)
+
+# Change variable value
+name = "Tom"
+print(name)
+
+# Multi-word variable name
+user_name = "admin"
+print(user_name)

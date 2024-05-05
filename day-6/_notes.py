d259fb44f263a332221d73e20a7d6f6936ea7ca6
Shandy Pranata Darmawan <38228367+shandypd@users.noreply.github.com>
2024-05-05 23:53:55 +0700

    Day 6 - Python Functions & Karel

---
+++ day-6/_notes.py
@@ -0,0 +1,13 @@
+# Define a new function
+def my_function(name):
+    print(f"Hello, {name}!")
+
+
+# Execute a function
+my_function("John")
+
+# While loop
+i = 0
+while i < 5:
+    i += 1
+    print(i)
